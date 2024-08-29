62ec370e55ff2e02c64efc2a46378bdc02c2dca1
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2024-08-29 18:28:38 -0400

    build: Integración de código para processing

--- sketch_aug14a.ino
+++ sketch_aug14a.ino
@@ -1,41 +1,51 @@
 // Definición de pines
 int buzzer = 12;  // Pin digital conectado al buzzer
 int analogPin = A0;  // Pin analógico conectado al sensor de monóxido de carbono (o cualquier sensor analógico)
+String alarm = "e"; // e de encendido
 
 // Configuración inicial
-void setup()
-{
+void setup() {
   Serial.begin(9600); // Inicia la comunicación serial a 9600 bps
   pinMode(buzzer, OUTPUT); // Configura el pin del buzzer como salida
 }
 
 // Bucle principal
-void loop()
-{
+void loop() {
   int val;
   val = analogRead(analogPin);  // Lee el valor del sensor conectado al pin A0
   Serial.println(val, DEC); // Imprime el valor leído en el monitor serial en formato decimal
 
   // Si el valor leído del sensor es mayor a 600, se activa la alarma
-  if (val > 600) {
+  if (val > 0) {
     unsigned char i;
 
     // Bucle infinito para activar el buzzer mientras el valor del sensor siga siendo alto
-    while (1) {
-      // Genera un sonido rápido en el buzzer
-      for (i = 0; i < 80; i++) {
-        digitalWrite(buzzer, HIGH);  // Activa el buzzer
-        delay(1);  // Espera 1 ms
-        digitalWrite(buzzer, LOW);  // Desactiva el buzzer
-        delay(1);  // Espera 1 ms
+    while (alarm == "e") {
+      // Verifica si hay un comando enviado desde Processing
+      if (Serial.available() > 0) {
+        char comando = Serial.read();
+        if (comando == 'a') {
+          alarm = "d"; // Cambia el estado de la alarma para detener el buzzer
+        }
       }
 
-      // Genera un sonido más lento en el buzzer
-      for (i = 0; i < 100; i++) {
-        digitalWrite(buzzer, HIGH);  // Activa el buzzer
-        delay(2);  // Espera 2 ms
-        digitalWrite(buzzer, LOW);  // Desactiva el buzzer
-        delay(2);  // Espera 2 ms
+      // Si la alarma sigue encendida, sigue sonando el buzzer
+      if (alarm == "e") {
+        // Genera un sonido rápido en el buzzer
+        for (i = 0; i < 80; i++) {
+          digitalWrite(buzzer, HIGH);  // Activa el buzzer
+          delay(1);  // Espera 1 ms
+          digitalWrite(buzzer, LOW);  // Desactiva el buzzer
+          delay(1);  // Espera 1 ms
+        }
+
+        // Genera un sonido más lento en el buzzer
+        for (i = 0; i < 100; i++) {
+          digitalWrite(buzzer, HIGH);  // Activa el buzzer
+          delay(2);  // Espera 3 ms
+          digitalWrite(buzzer, LOW);  // Desactiva el buzzer
+          delay(2);  // Espera 3 ms
+        }
       }
     }
   }
